778de81e5806d0c96052aad6da495a4374dc22a6
PrateekPathak9002 <89402723+PrateekPathak9002@users.noreply.github.com>
2021-11-22 18:57:08 +0530

    Update swifty.js

--- swifty.js
+++ swifty.js
@@ -1,29 +1,33 @@
-let elem;
-function css(selector,props) {
-	elem = document.querySelectorAll(selector)
+window.onload = function(){
+	let elem;
+	function css(selector,props) {
+		elem = document.querySelectorAll(selector)
 
-	for(var m = 0 ; m < elem.length ; m++ ){
-		for(var i in props){
-			eval(`elem[m].style.${i} = '${props[i]}' `)
+		for(var m = 0 ; m < elem.length ; m++ ){
+			for(var i in props){
+				eval(`elem[m].style.${i} = '${props[i]}' `)
+			}
+		}
+
+	}
+	function click(selector,functionToRun) {
+		elem = document.querySelectorAll(selector)
+		for(var i = 0 ; i < elem.length ; i++ ){
+			elem[i].addEventListener('click',functionToRun)
 		}
 	}
-	
-}
-function click(selector,functionToRun) {
-	elem = document.querySelectorAll(selector)
-	for(var i = 0 ; i < elem.length ; i++ ){
-		elem[i].addEventListener('click',functionToRun)
+
+	function hover(selector,functionToRun) {
+		elem = document.querySelectorAll(selector)
+		for(var i = 0 ; i < elem.length ; i++ ){
+			elem[i].addEventListener('mouseover',functionToRun)
+		}
+	}
+
+	let dropdowns = document.querySelectorAll(".sw-dropdown")
+
+	for(var i = 0 ; i < dropdowns.length ; i++ ){
+		console.log(dropdowns[i].className)
+		dropdowns[i].innerHTML += '<i class="fas fa-arrow-down down" aria-hidden="true"></i>';
 	}
 }
-
-function hover(selector,functionToRun) {
-	elem = document.querySelectorAll(selector)
-	for(var i = 0 ; i < elem.length ; i++ ){
-		elem[i].addEventListener('mouseover',functionToRun)
-	}
-}
-
-let dropdowns = document.getElementsByClassName('sw-dropdown')
-for(var i = 0 ; i < dropdowns.length ; i++ ){
-	dropdowns[i].append('<i class="fas fa-arrow-down down"></i>')
-	document.getElementsByClassName('down')[0].style.textAlign = 'right'
