887bfad2c3943090077047b547aa79ec39d05193
PrateekPathak9002 <89402723+PrateekPathak9002@users.noreply.github.com>
2021-11-24 15:43:45 +0530

    Update swifty.js

--- swifty.js
+++ swifty.js
@@ -29,6 +29,19 @@ window.onload = function(){
 	for(var i = 0 ; i < dropdowns.length ; i++ ){
 		console.log(dropdowns[i].className)
 		dropdowns[i].innerHTML += '<i class="fas fa-arrow-circle-down down " aria-hidden="true"></i>';
-		document.querySelectorAll(".down")[i].style.float = 'right'
 	}
+	$(".down").click(function (e) {
+		for(var i = 0 ; i < e.target.parentElement.parentElement.querySelectorAll('.sw-dropdown-options').length ; i++ ){
+			let element = e.target.parentElement.parentElement.querySelectorAll('.sw-dropdown-options')[i]
+			if (element.style.display == 'none') {
+				element.style.display = 'inline'
+				e.target.style.transform = 'rotate(0deg)'
+			}else{
+				element.style.display = 'none'
+				e.target.style.transform = 'rotate(-90deg)'
+			}
+		}
+		
+
+	});
 }
