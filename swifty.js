746de6d5aa54239d11d8fd1d4702297b296a1d13
PrateekPathak9002 <89402723+PrateekPathak9002@users.noreply.github.com>
2021-12-15 20:17:45 +0530

    Added data options

--- swifty.js
+++ swifty.js
@@ -70,5 +70,84 @@ window.onload = function(){
 			$('.sw-alert-dismiss').parent()[0].style.display = 'none'
 		}, 305);
 	});
+	
 
+	var parseData = (function(){
+
+		var getAllComments = function(context) {
+	
+				var ret = [],
+					node = context.firstChild;
+	
+				if (!node) { return ret; }
+	
+				do {
+					if (node.nodeType === 8) {
+						ret[ret.length] = node;
+					}
+					if (node.nodeType === 1) {
+						ret = ret.concat( getAllComments(node) );
+					}
+				} while( node = node.nextSibling );
+	
+				return ret;
+	
+			},
+			cache = [0],
+			expando = 'data' + +new Date(),
+			data = function(node) {
+	
+				var cacheIndex = node[expando],
+					nextCacheIndex = cache.length;
+	
+				if(!cacheIndex) {
+					cacheIndex = node[expando] = nextCacheIndex;
+					cache[cacheIndex] = {};
+				}
+	
+				return cache[cacheIndex];
+	
+			};
+	
+		return function(context) {
+	
+			context = context || document.documentElement;
+	
+			if ( data(context) && data(context).commentJSON ) {
+				return data(context).commentJSON;
+			}
+	
+			var comments = getAllComments(context),
+				len = comments.length,
+				comment, cData;
+	
+			while (len--) {
+				comment = comments[len];
+				cData = comment.data.replace(/\n|\r\n/g, '');
+				if ( /^\s*?\{.+\}\s*?$/.test(cData) ) {
+					try {
+						data(comment.parentNode).commentJSON = (new Function('return ' + cData + ';'))();
+					} catch(e) {}
+				}
+			}
+	
+			return data(context).commentJSON || true;
+	
+		};
+	
+	})();
+
+	for (let i = 0; i < document.querySelectorAll('.sw-alert-dismiss').length; i++) {
+		console.log(document.querySelectorAll('.sw-alert-dismiss') , i)
+		const element = document.querySelectorAll('.sw-alert-dismiss')[i];
+		if (parseData(element).bg_color !== undefined) {
+			let bgColor = parseData(element).bg_color
+			element.style.background = bgColor
+		}
+
+		if (parseData(element).color !== undefined) {
+			let color = parseData(element).color
+			element.style.color = color
+		}
+	}
 }
